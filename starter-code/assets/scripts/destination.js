17fd90739b4796e548d641f2fa849d92fbe12862
Pedro Vitor <pedrovitor2110@gmail.com>
2024-04-15 15:52:04 -0300

    feat: iniciando owl carosel para destination

---
+++ starter-code/assets/scripts/destination.js
@@ -0,0 +1,22 @@
+$('.container_carousel_planet .owl-carousel').owlCarousel({
+    loop:false,
+    margin:10,
+    nav:false,
+    autoWidth: true,
+    items: 1,
+})
+
+function namePlanetInOwlNav() {
+    const planets = ['Moon', 'Mars', 'Europe', 'Titan'];
+
+    let owlNav = document.querySelectorAll('.container_carousel_planet .owl-dot');
+
+    console.log(owlNav);
+
+    for(let i = 0; i < owlNav.length; i++){
+        console.log(i)
+        owlNav[i].innerHTML = planets[i];
+    }
+}
+
+namePlanetInOwlNav();
